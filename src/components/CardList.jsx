e9f5e81555d8be66c904038508ca2f26b0674fab
ArviKam-99 <shashikamal999@gmail.com>
2025-02-25 03:42:01 +0000

    added previous and next buttons

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,11 +1,16 @@
 import Card from './Card'
+import Button from './Button'
 
 const CardList = ({data}) => {
   return (
     <div className="cf pa2">
       <div className="mt2 mb2">
-      {data && data.map((prodcut) => 
+      {data && data.map((product) => 
         (<Card key={product.id} {...product} />))}
+      </div>
+      <div className="flex items-center justify-center pa4">   
+        <Button text="Previous" />
+        <Button text="Next" />
       </div>
     </div>
   )
